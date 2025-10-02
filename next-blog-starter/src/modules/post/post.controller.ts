0c99d3ed53e35c404646e4292282657932351f69
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-10-02 23:42:13 +0600

    implementation isFeatured filtering

--- next-blog-starter/src/modules/post/post.controller.ts
+++ next-blog-starter/src/modules/post/post.controller.ts
@@ -2,97 +2,104 @@ import { Request, Response } from "express";
 import { PostService } from "./post.service";
 
 const createPost = async (req: Request, res: Response) => {
-  try {
-    const result = await PostService.createPost(req.body);
+    try {
+        const result = await PostService.createPost(req.body);
 
-    res.status(201).json({
-      message: "Post Created Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+        res.status(201).json({
+            message: "Post Created Successfully",
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
 
-    res.status(500).json({
-      error: error,
-    });
-  }
+        res.status(500).json({
+            error: error,
+        });
+    }
 };
 
 const getAllPost = async (req: Request, res: Response) => {
-  try {
-    const result = await PostService.getAllPost();
+    try {
+        // const {page, limit} = req.query;    // req.query --->  er value hocche { page: '2', limit: '10' }
 
-    res.status(200).json({
-      message: "Post retrived Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const search = (req.query.search as string) || "";
+        const isFeatured = req.query.isFeatured ? req.query.isFeatured  === "true" : undefined;
 
-    res.status(500).json({
-      error: error,
-    });
-  }
+        const result = await PostService.getAllPost({ page, limit, search, isFeatured });
+
+        res.status(200).json({
+            message: "Post retrived Successfully",
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            error: error,
+        });
+    }
 };
 
 const getPostById = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const result = await PostService.getPostById(Number(id));
+    try {
+        const id = req.params.id;
+        const result = await PostService.getPostById(Number(id));
 
-    res.status(200).json({
-      message: "Post retrived Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+        res.status(200).json({
+            message: "Post retrived Successfully",
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
 
-    res.status(500).json({
-      error: error,
-    });
-  }
+        res.status(500).json({
+            error: error,
+        });
+    }
 };
 
 const updatePost = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const result = await PostService.updatePost(Number(id), req.body);
+    try {
+        const id = req.params.id;
+        const result = await PostService.updatePost(Number(id), req.body);
 
-    res.status(200).json({
-      message: "Post updated Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+        res.status(200).json({
+            message: "Post updated Successfully",
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
 
-    res.status(500).json({
-      error: error,
-    });
-  }
+        res.status(500).json({
+            error: error,
+        });
+    }
 };
 
 const deletePost = async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const result = await PostService.deletePost(Number(id));
+    try {
+        const id = req.params.id;
+        const result = await PostService.deletePost(Number(id));
 
-    res.status(200).json({
-      message: "Post deleted Successfully",
-      data: result,
-    });
-  } catch (error) {
-    console.log(error);
+        res.status(200).json({
+            message: "Post deleted Successfully",
+            data: result,
+        });
+    } catch (error) {
+        console.log(error);
 
-    res.status(500).json({
-      error: error,
-    });
-  }
+        res.status(500).json({
+            error: error,
+        });
+    }
 };
 
 export const PostController = {
-  createPost,
-  getAllPost,
-  getPostById,
-  updatePost,
-  deletePost,
+    createPost,
+    getAllPost,
+    getPostById,
+    updatePost,
+    deletePost,
 };
--- next-blog-starter/src/modules/post/post.service.ts
+++ next-blog-starter/src/modules/post/post.service.ts
@@ -2,70 +2,109 @@ import { Post, Prisma } from "@prisma/client";
 import { prisma } from "../../config/db";
 
 const createPost = async (payload: Prisma.PostCreateInput): Promise<Post> => {
-  const result = await prisma.post.create({
-    data: payload,
-    include: {
-      //---------see all info of user----------------
-      //author: true    // The Post model creates a relationship between the user and post using the author's name.
+    const result = await prisma.post.create({
+        data: payload,
+        include: {
+            //---------see all info of user----------------
+            //author: true    // The Post model creates a relationship between the user and post using the author's name.
 
-      author: {
-        //---------see specific info of user----------------
-        select: {
-          id: true,
-          name: true,
-          email: true,
+            author: {
+                //---------see specific info of user----------------
+                select: {
+                    id: true,
+                    name: true,
+                    email: true,
+                },
+            },
         },
-      },
-    },
-  });
-  return result;
+    });
+    return result;
 };
 
-const getAllPost = async () => {
-  const result = await prisma.post.findMany({
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
+//বাম দিকটা ( { page, limit } ) = destructuring (object থেকে value আলাদা করা)।
+// ডান দিকটা ( : { page: number; limit: number } ) = type checking (object কেমন হবে সেটা define করা)।
+const getAllPost = async ({
+    page,
+    limit,
+    search,
+    isFeatured,
+}: {
+    page: number;
+    limit: number;
+    search: string;
+    isFeatured?: boolean;
+}) => {
+    const skip = (page - 1) * limit;
 
-  return result;
-};
+    const where: any = {
+        AND: [
+            search && {
+                OR: [
+                    {
+                        title: {
+                            contains: search,
+                            mode: "insensitive",
+                        },
+                    },
+                    {
+                        content: {
+                            contains: search,
+                            mode: "insensitive",
+                        },
+                    },
+                ],
+            },
+            typeof isFeatured === "boolean" && {isFeatured: isFeatured},
+        ].filter(Boolean),
+    };
 
-const getPostById = async (id: number) => {
-  const result = await prisma.post.findUnique({
-    where: {
-      id,
-    },
-  });
-
-  return result;
-};
-
-const updatePost = async(id: number, payload: Partial<Post>) => {
-    const result = await prisma.post.update({
-        where: {
-            id
+    const result = await prisma.post.findMany({
+        skip,
+        take: limit,
+        where,
+        orderBy: {
+            createdAt: "desc",
         },
-        data: payload
     });
 
     return result;
-}
+};
 
-const deletePost = async(id: number) => {
-    const result = await prisma.post.delete({
+const getPostById = async (id: number) => {
+    const result = await prisma.post.findUnique({
         where: {
-            id
-        }
-    })
+            id,
+        },
+    });
 
     return result;
-}
+};
+
+const updatePost = async (id: number, payload: Partial<Post>) => {
+    const result = await prisma.post.update({
+        where: {
+            id,
+        },
+        data: payload,
+    });
+
+    return result;
+};
+
+const deletePost = async (id: number) => {
+    const result = await prisma.post.delete({
+        where: {
+            id,
+        },
+    });
+
+    return result;
+};
 
 export const PostService = {
-  createPost,
-  getAllPost,
-  getPostById,
-  updatePost,
-  deletePost
+    createPost,
+    getAllPost,
+    getPostById,
+    updatePost,
+    deletePost,
 };
